6fe2cc155f3625a04e609e11635fc051e9a92220
A Vaz <dev@andersonvaz.com>
2023-08-27 22:21:52 +0200

    Initial version.

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="grid gap-2">
+    <header class="text-center text-2xl font-bold text-green-400">
+      Seleção Marchador
+    </header>
+    <main>
+      <img src="images/sm1.jpeg" alt="Banner"/>
+    </main>
+    <footer>
+      <p class="text-center text-xl font-bold text-green-400">
+        Seja bem vindo ao nosso site!
+      </p>
+    </footer>
+  </div>  
+</template>
